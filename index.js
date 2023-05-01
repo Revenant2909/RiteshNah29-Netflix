692c4fe0a3a38203f655b6c9d89f5ce408f023a5
Ritesh <riteshnahavre2909@gmail.com>
2023-05-02 05:09:59 +0530

    First

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-const cors = require('cors');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
 dotenv.config();
-
-// app.use((req, res, next) => {
-//     res.header(Access-Control-Allow-Origin, "https://mern-netflix-frontend.onrender.com/");
-//     res.header(Access-Control-Allow-Methods, GET, POST, PUT, DELETE);
-//     res.header(Access-Control-Allow-Headers, Content-Type, Authorization);
-//     res.header(Access-Control-Allow-Credentials, true);
-//     next();
-//   });
+ 
+app.use(express.static('build'));
 mongoose
         .connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
@@ -29,9 +22,7 @@ mongoose
         app.use("/api/users", userRoute);
         app.use("/api/movies", movieRoute);
         app.use("/api/lists", listRoute);
-        app.use(cors({
-            origin: "https://mern-netflix-frontend.onrender.com/"
-          }));
+       
     
 app.listen(process.env.PORT || 8800 ,()=>{
     console.log("Backend server is running!");
